bc1f5f7da3cb4e8094af778039a177606c32fc18
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-03-21 11:13:58 -0500

    boards/cc1312-launchapd: misc documentation fix
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- boards/cc1312-launchpad/include/board.h
+++ boards/cc1312-launchpad/include/board.h
@@ -9,11 +9,11 @@
  */
 
 /**
- * @ingroup         boards_cc1352_launchpad
+ * @ingroup         boards_cc1312_launchpad
  * @{
  *
  * @file
- * @brief           Board specific definitions for TI CC1352 LaunchPad
+ * @brief           Board specific definitions for TI CC1312 LaunchPad
  *
  * @author          Nicholas Jackson <nicholas.jackson@griffithuni.edu.au>
  * @author          Sebastian Meiling <s@mlng.net>
